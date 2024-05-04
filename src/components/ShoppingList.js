56059c534dcd51dd1fd8bfa37eea13a666fa535b
flakesmonamour <mwangimark456@gmail.com>
2024-05-04 20:45:43 +0300

    initial commit

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,30 @@
-import React from "react";
-import Item from "./Item";
+import React,{useState} from 'react';
+import Item from './Item';
 
-function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+function ShoppingList({items}) {
+  const [selectedCategory, setSelectedCategor]= useState("All");
+  const[filteredItems , setFilteredItem]= useState([]);
+
+  function handleselect(e){
+    const category = e.target.value;
+    setSelectedCategor(category);
+    const filtered = category === "All" ? items : items.filter(
+      (item) => item.category === category
+    );
+    setFilteredItem(filtered);
+  }
+  return(
+    <div className='ShoppingList'>
+      <div className='Filter'>
+        <select name='filter' onChange={handleselect} value={selectedCategory}>
+          <option value='All'>Filter by category</option>
+          <option value='Produce'>Produce</option>
+          <option value='Dairy'>Dairy</option>
+          <option value='Dessert'>Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
+      <ul className='Items'>
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
